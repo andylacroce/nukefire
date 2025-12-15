c22982de48bb4eba12b28d22bc25184a29e18e17
Andy Lacroce <andy.lacroce@gmail.com>
2025-12-15 14:47:56 -0500

    Refactor character loading and combat scripts; update delays and streamline actions for improved performance

--- nukefire.bat
+++ nukefire.bat
@@ -1,11 +1,11 @@
 @echo off
 
 set "BIN=C:\Users\andyl\AppData\Roaming\WinTin++\bin"
-set "COLS=210"
-set "LINES=60"
 
 wt -M ^
   new-tab --title "Mutiny" -d "%BIN%" cmd /k ^
-    "mode con: cols=%COLS% lines=%LINES% & timeout /t 2 /nobreak > nul & \"%BIN%\tt++.exe\" -r \"nukefire\mutiny.tin\"" ^
+    "timeout /t 0 /nobreak > nul & \"%BIN%\tt++.exe\" -r \"nukefire\mutiny.tin\"" ^
   ; new-tab --title "Haenym" -d "%BIN%" cmd /k ^
-    "mode con: cols=%COLS% lines=%LINES% & timeout /t 2 /nobreak > nul & \"%BIN%\tt++.exe\" -r \"nukefire\haenym.tin\""
+    "timeout /t 5 /nobreak > nul & \"%BIN%\tt++.exe\" -r \"nukefire\haenym.tin\"" ^
+  ; new-tab --title "Prodigy" -d "%BIN%" cmd /k ^
+    "timeout /t 10 /nobreak > nul & \"%BIN%\tt++.exe\" -r \"nukefire\prodigy.tin\""
