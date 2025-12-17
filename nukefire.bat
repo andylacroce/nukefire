7c8d290a4aa42aade89e4d9890934d6d34481804
Andy Lacroce <andy.lacroce@gmail.com>
2025-12-17 16:53:11 -0500

    Update Mutiny tab timeout to 3 seconds for improved loading experience

--- nukefire.bat
+++ nukefire.bat
@@ -4,7 +4,7 @@ set "BIN=C:\Users\andyl\AppData\Roaming\WinTin++\bin"
 
 wt -M ^
   new-tab --title "Mutiny" -d "%BIN%" cmd /k ^
-    "timeout /t 0 /nobreak > nul & \"%BIN%\tt++.exe\" -r \"nukefire\mutiny.tin\"" ^
+    "timeout /t 3 /nobreak > nul & \"%BIN%\tt++.exe\" -r \"nukefire\mutiny.tin\"" ^
   ; new-tab --title "Haenym" -d "%BIN%" cmd /k ^
     "timeout /t 5 /nobreak > nul & \"%BIN%\tt++.exe\" -r \"nukefire\haenym.tin\"" ^
   ; new-tab --title "Prodigy" -d "%BIN%" cmd /k ^
